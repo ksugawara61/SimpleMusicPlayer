8c2b954f0e4cf8175ead0289c5ffc73df3aedb9f
ksugawara61 <katsuo.com61@gmail.com>
2016-07-21 23:58:09 +0900

    ボタンのクリックイベントのドライバを作成

--- app/src/main/java/org/example/simplemusicplayer/MainFragment.java
+++ app/src/main/java/org/example/simplemusicplayer/MainFragment.java
@@ -37,6 +37,74 @@ public class MainFragment extends Fragment {
             }
         });
 
+        // 前へボタンのイベントを作成
+        View prev_button = root_view.findViewById(R.id.prev_button);
+        prev_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // ここで前の曲を再生
+
+                AlertDialog.Builder builder =
+                        new AlertDialog.Builder(getActivity());
+                builder.setTitle("prev music");
+                builder.setMessage("Now, You play the music");
+                builder.setCancelable(false);
+                builder.setPositiveButton("OK", null);
+                m_dialog = builder.show();
+            }
+        });
+
+        // 次へボタンのイベントを作成
+        View next_button = root_view.findViewById(R.id.next_button);
+        next_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // ここで次の曲を再生
+
+                AlertDialog.Builder builder =
+                        new AlertDialog.Builder(getActivity());
+                builder.setTitle("next music");
+                builder.setMessage("Now, You play the music");
+                builder.setCancelable(false);
+                builder.setPositiveButton("OK", null);
+                m_dialog = builder.show();
+            }
+        });
+
+        // ループボタンのイベントを作成
+        View roop_button = root_view.findViewById(R.id.roop_button);
+        roop_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // ここでループのON/OFF設定
+
+                AlertDialog.Builder builder =
+                        new AlertDialog.Builder(getActivity());
+                builder.setTitle("roop music");
+                builder.setMessage("Now, You play the music");
+                builder.setCancelable(false);
+                builder.setPositiveButton("OK", null);
+                m_dialog = builder.show();
+            }
+        });
+
+        // シャッフルボタンのイベントを作成
+        View shuffle_button = root_view.findViewById(R.id.shuffle_button);
+        shuffle_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // ここでループのON/OFF設定
+
+                AlertDialog.Builder builder =
+                        new AlertDialog.Builder(getActivity());
+                builder.setTitle("shuffle music");
+                builder.setMessage("Now, You play the music");
+                builder.setCancelable(false);
+                builder.setPositiveButton("OK", null);
+                m_dialog = builder.show();
+            }
+        });
+
         return root_view;
     }
 }
