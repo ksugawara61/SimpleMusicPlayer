46456d2659e7064d3ee7b4f3bf854ef39478062d
ksugawara61 <katsuo.com61@gmail.com>
2016-07-30 21:11:43 +0900

    設定のアクティビティへ遷移する処理を実装

--- app/src/main/java/org/example/simplemusicplayer/Controller/MainActivity.java
+++ app/src/main/java/org/example/simplemusicplayer/Controller/MainActivity.java
@@ -1,5 +1,6 @@
 package org.example.simplemusicplayer.Controller;
 
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,17 +8,20 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.example.simplemusicplayer.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     private AlertDialog m_dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
     }
 
@@ -28,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
      */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d(TAG, "onCreateOptionsMenu");
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -39,9 +44,11 @@ public class MainActivity extends AppCompatActivity {
      */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        Log.d(TAG, "onOptionsItemSelected");
         switch(item.getItemId()) {
             case R.id.main_settings:
-                Toast.makeText(this, "設定がタップされました", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this, SettingActivity.class);
+                startActivity(intent);
                 break;
             case R.id.main_help:
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
--- app/src/main/java/org/example/simplemusicplayer/Controller/SettingActivity.java
+++ app/src/main/java/org/example/simplemusicplayer/Controller/SettingActivity.java
@@ -2,18 +2,23 @@ package org.example.simplemusicplayer.Controller;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import org.example.simplemusicplayer.R;
 
 /**
  * Created by katsuya on 16/07/30.
+ * 設定画面のアクティビティ
  */
 public class SettingActivity extends AppCompatActivity {
+
+    private static final String TAG = "SettingActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+        setContentView(R.layout.activity_setting);
     }
 
 }
