8ace661ebe9f2a334085b614085ac2a1fdb09ad5
ksugawara61 <katsuo.com61@gmail.com>
2016-07-30 20:07:12 +0900

    タイトルバーを表示しメニューボタンを追加

--- app/src/main/java/org/example/simplemusicplayer/Controller/MainActivity.java
+++ app/src/main/java/org/example/simplemusicplayer/Controller/MainActivity.java
@@ -3,6 +3,7 @@ package org.example.simplemusicplayer.Controller;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.widget.Button;
 
 import org.example.simplemusicplayer.R;
@@ -15,5 +16,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
